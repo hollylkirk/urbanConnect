5a13cc420603526d6c86e70989e9a474eda11b9b
Holly Kirk <hollylkirk@gmail.com>
2019-06-07 17:03:35 +1000

    adds the new R script files to the repo

---
+++ R/scripts/connectFunctions.R
@@ -0,0 +1,9 @@
+# functions!
+
+# load GIS data layer and create a spatial object
+# with the geometry in it
+loadLayer <- function(filename, ...){
+  # things
+  # layer <- things
+  return(layer)
+}
---
+++ R/scripts/testingWorkFlow.R
@@ -0,0 +1,24 @@
+rm(list = ls())
+# general script to load the raw database exported data for an area site, 
+# filter and reshape accordingly, export as csvs for use in later scripts
+
+library(here)
+library(sp)
+library(sf)
+library(rgeos)
+# library(tidyverse)
+
+# where are the functions
+source(here("scripts/connectFunctions.R"))
+
+# set the outfolder
+outfolder <- here("data/derivedData/")
+
+
+
+# load habitat layer
+hab <- st_read(here("data/GISdata/PhaseOne_files/AllVegetation/", "AllVeg.shp"))
+
+# load barrier layer
+bar <- st_read(here("data/GISdata/PhaseOne_files/AllVegetation/", "AllBarriers10m_75mBuffDiss.shp"))
+
